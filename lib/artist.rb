908e6ff9f56b387e6a7a7ac7df9386f90387db00
George Blackmore <gtblackmore@gmail.com>
2020-01-12 17:01:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,22 @@
 class Artist
-  attr_accessor :name
 
-  @@all = []
+  attr_accessor :name
 
   def initialize(name)
     @name = name
-    @@all << self
   end
 
-  def self.all
-    @@all
+  def songs
+    Song.all
   end
 
   def add_song(song)
     song.artist = self
   end
 
-  def add_song_by_name(name)
-    song = Song.new(name)
-    song.artist = self
-  end
-
-  def songs
-    Song.all.select {|song| song.artist == self}
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    add_song(song)
   end
 
   def self.song_count
