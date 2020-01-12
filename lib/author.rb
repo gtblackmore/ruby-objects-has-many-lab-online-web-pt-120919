f299867737aabc848b2efce23dfd7f7abcb2bcaf
George Blackmore <gtblackmore@gmail.com>
2020-01-12 16:24:03 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,7 +7,13 @@ class Author
   end
   
   def add_post(post)
-    post.author = post
+    @posts << post
+    post.author = self
+  end
+  
+  def add_post_by_title(post)
+    post.new = post
+    
 
 
 end
