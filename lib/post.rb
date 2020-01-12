8bc842f1bf351f26763d892e2e68334360440786
George Blackmore <gtblackmore@gmail.com>
2020-01-12 17:03:09 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,18 +1,23 @@
 class Post
-  attr_accessor :title, :author
-  
+
   @@all = []
-  
-  def initalize(title)
-    @title = title
+
+  attr_accessor :author,:title
+
+  def initialize(message)
+    @title = message
     @@all << self
   end
-  
-  def author_name
-    author.name if author
-  end
-  
+
   def self.all
     @@all
   end
+
+  def author_name
+    if self.author
+      return self.author.name
+    end
+    nil
+  end
+
 end
