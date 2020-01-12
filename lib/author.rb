7ed927bad0ef8876f83aedc2361abb2c46bb9c56
George Blackmore <gtblackmore@gmail.com>
2020-01-12 17:01:57 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,31 +1,26 @@
 class Author
-  attr_accessor :name
 
-  @@all = []
+  attr_accessor :name
 
   def initialize(name)
     @name = name
-    @@all << self
   end
 
-  def self.all
-    @@all
+  def posts
+    Post.all
   end
 
   def add_post(post)
     post.author = self
   end
 
-  def add_post_by_title(title)
-    post = Post.new(title)
-    post.author = self
-  end
-
-  def posts
-    Post.all.select {|post| post.author == self}
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    add_post(post)
   end
 
   def self.post_count
     Post.all.count
   end
-end
+
+end 
